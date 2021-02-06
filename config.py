fc503a3d3b8e3833206cdd14dc8270cae5b5cbd3
pforderique <porderiq@mit.edu>
2021-02-06 09:36:27 -0500

    init commit

---
+++ config.py
@@ -0,0 +1,25 @@
+'''
+Configuration File
+
+Configure your preferences here for:
+    Twitter account keys and token
+    celebrity
+    email recipients
+'''
+
+# TWITTER DEVELOPER KEYS HERE
+consumer_key = '...',
+consumer_secret = '...'
+access_token = '...'
+access_token_secret = '...'
+
+# CELEBRITY OF CHOICE
+CELEBRITY = 'Elon Musk'
+
+# EMAIL RECIPIENTS
+EMAIL_RECIPIENTS = [
+    'example@gmail.com',
+    'johnnyappleseed@gmail.com',
+    '...',
+]
+# MAKE SURE TO TURN ON 'LESS SECURE ACCESS' FOR GMAIL ACCOUNTS
